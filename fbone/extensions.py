a4d324d12e7192568b4b47ba8df9fcd2eb62a26f
imwilsonxu <im.wilson.xu@gmail.com>
2012-11-20 16:24:59 +0800

    fix import flask cache.

--- fbone/extensions.py
+++ fbone/extensions.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 from flask.ext.mail import Mail
 mail = Mail()
 
-from flaskext.cache import Cache
+from flask.ext.cache import Cache
 cache = Cache()
 
 from flask.ext.login import LoginManager
